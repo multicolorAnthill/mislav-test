7aeb4c22078cfa5e1038a7e30c6d1b2cdc199972
misla <misla@DESKTOP-CSDNVGE>
2017-02-06 23:48:47 +0100

    Dodana dva komentara i jedan ispis

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,6 +3,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Kako si?");
+		// bok kako si 
+		
+		//ja sam jako dobro
+		System.out.println("Ne zovi me...");
 	}
 
 }
